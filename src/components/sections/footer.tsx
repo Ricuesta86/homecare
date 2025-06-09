98db3c951a09dbc0c1b60903f01f2828a0bdf7e6
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2025-06-09 16:25:12 -0400

    Actualizando el footer

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,29 +1,41 @@
+import {
+  Clock,
+  Facebook,
+  Instagram,
+  Mail,
+  MapPin,
+  Phone,
+  Twitter,
+} from "lucide-react";
+import Link from "next/link";
+
 export function Footer() {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-blue-800 text-white pt-10 pb-6">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Columna 1 - Información */}
           <div>
-            <h3 className="text-xl font-bold mb-4">HomeCare</h3>
+            <h3 className="text-xl font-bold mb-4">Latin Angels</h3>
             <p className="mb-4">
               Brindamos cuidado profesional y compasivo en la comodidad de su
               hogar.
             </p>
             <div className="flex space-x-4">
-              <a href="#" className="hover:text-blue-200 transition">
+              <Link href={"/"} className="hover:text-blue-200 transition">
                 <span className="sr-only">Facebook</span>
-                <i className="fab fa-facebook text-2xl"></i>
-              </a>
-              <a href="#" className="hover:text-blue-200 transition">
+                <i></i>
+                <Facebook className="text-2xl" />
+              </Link>
+              <Link href="#" className="hover:text-blue-200 transition">
                 <span className="sr-only">Twitter</span>
-                <i className="fab fa-twitter text-2xl"></i>
-              </a>
-              <a href="#" className="hover:text-blue-200 transition">
+                <Twitter className=" text-2xl" />
+              </Link>
+              <Link href="#" className="hover:text-blue-200 transition">
                 <span className="sr-only">Instagram</span>
-                <i className="fab fa-instagram text-2xl"></i>
-              </a>
+                <Instagram />
+              </Link>
             </div>
           </div>
 
@@ -32,29 +44,35 @@ export function Footer() {
             <h4 className="text-lg font-semibold mb-4">Enlaces Rápidos</h4>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href={"/"} className="hover:text-blue-200 transition">
                   Inicio
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link
+                  href="/services"
+                  className="hover:text-blue-200 transition"
+                >
                   Servicios
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="/about" className="hover:text-blue-200 transition">
                   Sobre Nosotros
-                </a>
+                </Link>
               </li>
-              <li>
+              {/* <li>
                 <a href="#" className="hover:text-blue-200 transition">
                   Testimonios
                 </a>
-              </li>
+              </li> */}
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link
+                  href="/contacto"
+                  className="hover:text-blue-200 transition"
+                >
                   Contacto
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -64,29 +82,29 @@ export function Footer() {
             <h4 className="text-lg font-semibold mb-4">Nuestros Servicios</h4>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="#" className="hover:text-blue-200 transition">
                   Cuidado de Ancianos
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="#" className="hover:text-blue-200 transition">
                   Asistencia Médica
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="#" className="hover:text-blue-200 transition">
                   Cuidado Postoperatorio
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="#" className="hover:text-blue-200 transition">
                   Acompañamiento
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:text-blue-200 transition">
+                <Link href="#" className="hover:text-blue-200 transition">
                   Servicios de Enfermería
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -95,20 +113,30 @@ export function Footer() {
           <div>
             <h4 className="text-lg font-semibold mb-4">Contacto</h4>
             <address className="not-italic">
-              <p className="mb-2">
-                <i className="fas fa-map-marker-alt mr-2"></i>
-                123 Calle Cuidado, Ciudad Hogar
+              <p className="mb-2 flex gap-1">
+                {/* <i className="fas fa-map-marker-alt mr-2"></i> */}
+                <MapPin />
+                7619 Coot St, Orlando, FL 32822
               </p>
-              <p className="mb-2">
-                <i className="fas fa-phone mr-2"></i>
-                (123) 456-7890
+              <p className="mb-2 flex gap-1">
+                {/* <i className="fas fa-phone mr-2"></i> */}
+                <Phone />
+                (786) 238-5632
               </p>
-              <p className="mb-2">
-                <i className="fas fa-envelope mr-2"></i>
-                info@homecare.com
+              <p className="mb-2 flex gap-1">
+                {/* <i className="fas fa-phone mr-2"></i> */}
+                <Phone />
+                (689) 302-5799
               </p>
-              <p>
-                <i className="fas fa-clock mr-2"></i>
+              <p className="mb-2 flex gap-1">
+                {/* <i className="fas fa-envelope mr-2"></i>
+                 */}
+                <Mail />
+                latinangelshomecare@gmail.com
+              </p>
+              <p className="mb-2 flex gap-1">
+                {/* <i className="fas fa-clock mr-2"></i> */}
+                <Clock />
                 24/7 Disponible
               </p>
             </address>
@@ -116,7 +144,10 @@ export function Footer() {
         </div>
 
         <div className="border-t border-blue-700 mt-8 pt-6 text-center">
-          <p>&copy; {currentYear} HomeCare. Todos los derechos reservados.</p>
+          <p>
+            &copy; {currentYear} Latin Angels for HomeCare. Todos los derechos
+            reservados.
+          </p>
           <div className="mt-2 text-sm">
             <a href="#" className="hover:text-blue-200 transition mx-2">
               Política de Privacidad
